23789fe84ed7a6cb419469e73c4c83e8f7db0313
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-06-25 19:06:24 +0530

    Setup Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl">Hello I am built with TailwindCSS</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+};
+
+export default App;
